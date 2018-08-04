eec258387810b0b9c548c0569e2f60316579a55c
fang-ng4 <907648506@qq.com>
2018-08-04 10:55:56 +0800

    就是这个的哦

--- test.js
+++ test.js
@@ -10,3 +10,9 @@ c.map(function (a) {
 c.reduce(function(a,b){
     return a+b
 })
+
+// 再来写点什么 来验证 git checkout . 的功能有什么用   
+// 验证结果： git checkout .（回退所有 git checkout --<file> 回退指定文件）  回退所有工作区的修改　，即还没有git add (暂存区)前的修改
+
+// 验证 git reset HEAD . 的功能效果
+// 验证结果： git reset HEAD . 将本地commit 的内容 回退到的暂存区 即 git add 之后的状态  
