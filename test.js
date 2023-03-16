2a4a0d0b124755131a3205c3b221c6f6d3d96572
fangliang <fangliang@qq.com>
2023-03-16 14:44:07 +0800

    测试git subtree

--- test.js
+++ test.js
@@ -1,28 +1,27 @@
-var c = [1, 2, 3, 4, 5]
+var c = [1, 2, 3, 4, 5];
 
 // 这是一个 本地暂存的库存，可以随时提取
 // 这是一个 本地暂存的库存，可以随时提取
-
+console.log(11);
 c.map(function (a) {
-    return a + 2
-})
+  return a + 2;
+});
 // 再写一个函数
 
 c.reduce(function (a, b) {
-    return a + b
-})
+  return a + b;
+});
 
-// 再来写点什么 来验证 git checkout . 的功能有什么用   
+// 再来写点什么 来验证 git checkout . 的功能有什么用
 // 验证结果： git checkout .（回退所有 git checkout --<file> 回退指定文件）  回退所有工作区的修改
 
 // 验证 git reset HEAD . 的功能效果
-// 验证结果： git reset HEAD . 将本地git add 的内容 回退到的工作区  
-
+// 验证结果： git reset HEAD . 将本地git add 的内容 回退到的工作区
 
 // 做 cherry-pick 有什么用
 
 // git cherry-pick <target-commit-id>命令是只将target-commit-id这一个提交提取到当前分支上
 
-console.log(111)
+console.log(111);
 // console.log = function() {}
 //asdfsdfadsf
